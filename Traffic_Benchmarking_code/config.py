458ee36a4a4c2fc6b3871adc138f32a5970dbc74
shilpa <shilpasingh5390@gmail.com>
2017-12-08 18:25:14 +0530

    Hello

---
+++ Traffic_Benchmarking_code/config.py
@@ -0,0 +1,8 @@
+API_path = {'MMI': 'http://traffic.mapmyindia.com/mmi/eta?path=',
+
+            'Google': 'https://maps.googleapis.com/maps/api/directions/json?origin=&destination=&key'
+                      '=AIzaSyDacet-jtT4WZuAyn9hdsKimr-ROczcjTI',
+
+            'Here': 'https://route.cit.api.here.com/routing/7.2/calculateroute.json?app_id=wiNy4xopFolIu8273dxu'
+                    '&app_code=pr0JL-P14WpZpj7k0L7EpA&waypoint0=geo!&waypoint1=geo!&mode=fastest;car;traffic:enabled'}
+
