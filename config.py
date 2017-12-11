ddb5511f6fb3e9f8622ec832b4abc5ca924ba72a
shilpa <shilpasingh5390@gmail.com>
2017-12-11 11:56:03 +0530

    Hello

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 API_path = {'MMI': 'http://traffic.mapmyindia.com/mmi/eta?path=',
 
             'Google': 'https://maps.googleapis.com/maps/api/directions/json?origin=&destination=&key'
-                      '=AIzaSyBxY5x5PEIGVf2U_hAjQoAgds8LEdqyHvM',
+                      '=AIzaSyC1Vch7eMW_WD4Je6eF21G8WVqEASVO5s4',
 
             'Here': 'https://route.cit.api.here.com/routing/7.2/calculateroute.json?app_id=wiNy4xopFolIu8273dxu'
                     '&app_code=pr0JL-P14WpZpj7k0L7EpA&waypoint0=geo!&waypoint1=geo!&mode=fastest;car;traffic:enabled'}
